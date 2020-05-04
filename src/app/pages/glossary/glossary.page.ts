06b9408840bb71932800d9ad89612e59b63be4bb
Anthony AUMETTRE <antho33@gmail.com>
2020-05-04 20:14:07 +0200

    Page Lexique Partie 5

--- src/app/pages/glossary/glossary.page.ts
+++ src/app/pages/glossary/glossary.page.ts
@@ -29,55 +29,70 @@ export class GlossaryPage implements OnInit {
       this.instrumentsList = this.instruments;
   }
 
-    loadAllSounds() {
-        this.files = new Array();
-        this.instruments.forEach((intru, i) => {
-            const uri = intru.sound;
+  loadAllSounds() {
+      this.files = new Array();
+      this.instruments.forEach((intru, i) => {
+          const uri = intru.sound;
 
-            if (this.platform.is('android')) {
-                this.files[intru.id] = this.media.create('/android_asset/public/' + uri);
-            }
-            if (this.platform.is('ios')) {
-                this.files[intru.id] = this.media.create('/android_asset/public/' + uri);
-            }
-        });
-    }
-
-    play(intruId: number, indexFile: number) {
-      if (this.instrumentsList[indexFile].playing) {
-          // Stop the sound
-          this.files[intruId].stop();
-          clearInterval(this.interval);
-          this.instrumentsList[indexFile].playing = false;
-          this.instrumentsList[indexFile].percent = 0;
-      } else {
-          // Play the sound
-          this.files[intruId].play();
-          clearInterval(this.interval);
-          this.interval = setInterval(() => {
-              this.files[intruId].getCurrentPosition().then((position) => {
-                  this.instrumentsList[indexFile].percent = position / this.files[intruId].getDuration();
-              });
-          }, 50);
-          this.instrumentsList[indexFile].playing = true;
-      }
-    }
-
-    filterList(evt) {
-      const searchTerm = evt.srcElement.value;
-      if (!searchTerm) {
-          this.instrumentsList = this.instruments;
-          return;
-      }
-      this.instrumentsList = this.instruments.filter(currentInstru => {
-          if (currentInstru.label && searchTerm) {
-              if (currentInstru.label.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
-                  return true;
-              }
-              return false;
+          if (this.platform.is('android')) {
+              this.files[intru.id] = this.media.create('/android_asset/public/' + uri);
+          }
+          if (this.platform.is('ios')) {
+              this.files[intru.id] = this.media.create('/android_asset/public/' + uri);
           }
       });
+  }
+
+  play(intruId: number, indexFile: number) {
+    if (this.instrumentsList[indexFile].playing) {
+        // Stop the sound
+        this.files[intruId].stop();
+        clearInterval(this.interval);
+        this.instrumentsList[indexFile].playing = false;
+        this.instrumentsList[indexFile].percent = 0;
+    } else {
+        // Play the sound
+        this.files[intruId].play();
+        clearInterval(this.interval);
+        this.interval = setInterval(() => {
+            this.files[intruId].getCurrentPosition().then((position) => {
+                this.instrumentsList[indexFile].percent = position / this.files[intruId].getDuration();
+            });
+        }, 50);
+        this.instrumentsList[indexFile].playing = true;
     }
+  }
+
+  filterList(evt) {
+    const searchTerm = evt.srcElement.value;
+    if (!searchTerm) {
+        this.instrumentsList = this.instruments;
+        return;
+    }
+    this.instrumentsList = this.instruments.filter(currentInstru => {
+        if (currentInstru.label && searchTerm) {
+            if (currentInstru.label.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+                return true;
+            }
+            return false;
+        }
+    });
+  }
+
+  filterSegment(evt) {
+    console.log(evt.detail.value);
+    const family = evt.detail.value;
+    if (family === 'all') {
+        this.instrumentsList = this.instruments;
+    } else {
+        this.instrumentsList = this.instruments.filter(currentInstru => {
+            if (currentInstru.family.label.toLowerCase() === family) {
+                    return true;
+            }
+            return false;
+        });
+    }
+  }
 
   ngOnInit() {
   }
